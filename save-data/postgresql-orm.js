fe21ea1b52495ebd252bf9e74010455c2c9f0248
Erik Larsen <eriklarsen@Eriks-MacBook-Air.local>
2017-12-01 21:09:02 -0800

    created postgres db. having issues with getting it to show up

---
+++ save-data/postgresql-orm.js
@@ -0,0 +1,57 @@
+const Sequelize = require('sequelize');
+const request = require('request');
+const API_URL = 'https://api.giphy.com/v1/gifs/search?api_key=MW5czB76HwkBu7TtxFZ4brmlICTLCLNA&q=cat&limit=200&offset=0&rating=G&lang=en'
+
+const sequelize = new Sequelize('solo_db', 'username', 'password', {
+  host: 'localhost',
+  dialect: 'postgres',
+});
+
+// sequelize.authenticate().then(() => {
+//   console.log('Connection has been established successfully.');
+// })
+// .catch(err => {
+//   console.error('Unable to connect to the database:', err);
+// });
+
+const GifEntry = sequelize.define('gifEntry', {
+  _id: {type: Sequelize.INTEGER, primaryKey: true, autoIncrement: true},
+  id: Sequelize.STRING,
+  imageLink: Sequelize.STRING,
+  smallImageLink: Sequelize.STRING,
+  smallFixedImageLink: Sequelize.STRING,
+  counter: {type: Sequelize.INTEGER, defaultValue: 0},
+  createdAt: {type: Sequelize.DATE, defaultValue: sequelize.literal('NOW()')},
+  tags: Sequelize.ARRAY
+});
+
+const Update = sequelize.define('update', {
+  _id: {type: Sequelize.INTEGER, primaryKey: true, autoIncrement: true},
+  lastUpdate: Sequelize.DATE
+});
+
+const getTags = (data) => {
+  return data.split(' ');
+}
+
+const migrate = (data) => {
+  sequelize.sync().then(() => {
+    data.data.forEach(entry => {
+      GifEntry.create({
+        id: entry.id,
+        imageLink: entry.images.fixed_width.url,
+        smallImageLink: entry.images.fixed_width_small.url,
+        smallFixedImageLink: entry.images.fixed_width_small_still.url,
+        counter: 0,
+        createdAt: new Date(Date.now()),
+        tags: getTags(entry.images.title)
+      });
+    });
+  });
+}
+
+request(API_URL, (err, resp) => {
+  let data = JSON.parse(resp.body);
+  console.log('data from API is ', data);
+  migrate(data);
+});
